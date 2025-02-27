bf6614e807b9e1a962fd6b834ebe90fa12d507db
GiulioFardella <giulio.fardella@outlook.it>
2025-02-27 21:38:44 +0100

    adds mods

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,65 @@
-
-
+import { Container, Row, Col } from "react-bootstrap";
+import "../css/footer.css"
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full bg-gray-900 text-white text-sm py-6 px-4 md:px-12">
-      <div className="container mx-auto grid grid-cols-2 md:grid-cols-4 gap-6">
-        {/* Informazioni Aziendali */}
-        <div>
-          <h3 className="text-lg font-semibold">ZooVersee</h3>
-          <p>Via Po 25/c, 00198 Roma</p>
-          <p>Telefono: 06844971</p>
-          <p>P.IVA: IT 02121111005</p>
-          <p>C.F: 80078430586</p>
-        </div>
+    <footer className="bg-dark text-white py-4 mt-auto generalFooter">
+      <Container className="footer-container">
+        <Row className="text-center text-md-start">
+          {/* Informazioni Aziendali */}
+          <Col md={3} className="mb-3">
+            <h5>ZooVersee</h5>
+            <p>Via Po 25/c, 00198 Roma</p>
+            <p>Telefono: 06844971</p>
+            <p>P.IVA: IT 02121111005</p>
+            <p>C.F: 80078430586</p>
+          </Col>
 
-        {/* Link Utili */}
-        <div>
-          <h3 className="text-lg font-semibold">Chi siamo</h3>
-          <ul className="space-y-1">
-            <li><a href="#" className="hover:underline">Cosa facciamo</a></li>
-            <li><a href="#" className="hover:underline">Dove interveniamo</a></li>
-            <li><a href="#" className="hover:underline">Specie e habitat</a></li>
-            <li><a href="#" className="hover:underline">Cosa puoi fare tu</a></li>
-            <li><a href="#" className="hover:underline">Pandanews</a></li>
-          </ul>
-        </div>
+          {/* Link Utili */}
+          <Col md={3} className="mb-3">
+            <h5>Chi siamo</h5>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-white text-decoration-none">Cosa facciamo</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Dove interveniamo</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Specie e habitat</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Cosa puoi fare tu</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Pandanews</a></li>
+            </ul>
+          </Col>
 
-        {/* Documentazione e Legale */}
-        <div>
-          <h3 className="text-lg font-semibold">Supporto</h3>
-          <ul className="space-y-1">
-            <li><a href="#" className="hover:underline">Area stampa</a></li>
-            <li><a href="#" className="hover:underline">Lavora con noi</a></li>
-            <li><a href="#" className="hover:underline">Newsletter</a></li>
-            <li><a href="#" className="hover:underline">Termini e condizioni</a></li>
-            <li><a href="#" className="hover:underline">Privacy policy</a></li>
-            <li><a href="#" className="hover:underline">Configurazione Cookies</a></li>
-          </ul>
-        </div>
+          {/* Documentazione e Legale */}
+          <Col md={3} className="mb-3">
+            <h5>Supporto</h5>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-white text-decoration-none">Area stampa</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Lavora con noi</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Newsletter</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Termini e condizioni</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Privacy policy</a></li>
+              <li><a href="#" className="text-white text-decoration-none">Configurazione Cookies</a></li>
+            </ul>
+          </Col>
 
-        {/* Social Media */}
-        <div>
-          <h3 className="text-lg font-semibold">Seguici</h3>
-          <div className="flex space-x-4 mt-2">
-            <a href="#" className="hover:text-blue-500">Facebook </a>
-            <a href="#" className="hover:text-blue-400">Twitter </a>
-            <a href="#" className="hover:text-pink-500">Instagram </a>
-            <a href="#" className="hover:text-blue-700">Linkedin </a>
-            <a href="#" className="hover:text-red-600">Youtube</a>
-          </div>
-        </div>
-      </div>
+          {/* Social Media */}
+          <Col md={3} className="mb-3">
+            <h5>Seguici</h5>
+            <div className="d-flex gap-3 justify-content-center justify-content-md-start">
+              <a href="#" className="text-white">Facebook</a>
+              <a href="#" className="text-white">Twitter</a>
+              <a href="#" className="text-white">Instagram</a>
+              <a href="#" className="text-white">LinkedIn</a>
+              <a href="#" className="text-white">YouTube</a>
+            </div>
+          </Col>
+        </Row>
 
-      {/* Copyright */}
-      <div className="mt-6 text-center text-xs border-t border-gray-700 pt-4">
-        <p> ZooVersee 2025 - Costruiamo un mondo in cui le persone possano vivere in armonia con la natura</p>
-        <p className="text-gray-400">Web design: Giulio Fardella</p>
-      </div>
+        {/* Copyright */}
+        <Row className="mt-4 border-top pt-3 text-center">
+          <Col>
+            <p className="mb-0">ZooVersee 2025 - Costruiamo un mondo in cui le persone possano vivere in armonia con la natura</p>
+            <p className="text-muted">Web design: Giulio Fardella</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
